cbb8f1d724de17c88c5d28e2b34dd76b8d11429f
unknown <bbabines@gmail.com>
2023-02-01 13:27:21 -0500

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
 	return (
 		<>
 			<Head>
-				<title>Next.JS Boilerplate</title>
+				<title>Brad's Portfolio</title>
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 			</Head>
 			<main>
