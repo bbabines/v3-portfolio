387f33f66f659d0968caf07cd27ea932135d796b
unknown <bbabines@gmail.com>
2023-02-06 00:15:57 -0500

    Added media queries for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import { IoMoonOutline, IoPartlySunnyOutline } from "react-icons/io5";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
+	const [darkmode, setDarkmode] = useState(true);
+
 	return (
 		<div className={styles.container}>
 			<p className={styles.signature}>Brad Babines</p>
-			<IoMoonOutline className={styles.navLinks} />
-			<IoPartlySunnyOutline className={styles.navLinks} />
-			<p className={styles.navLinks}>Home</p>
+
+			{darkmode ? (
+				<IoPartlySunnyOutline className={styles.navLinks} />
+			) : (
+				<IoMoonOutline className={styles.navLinks} />
+			)}
+
+			<p className={styles.homeLink}>Home</p>
 			<p className={styles.navLinks}>About</p>
 			<p className={styles.navLinks}>Skills</p>
 			<p className={styles.navLinks}>Projects</p>
