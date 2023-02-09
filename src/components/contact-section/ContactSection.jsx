754c9c882a666a304cfb8fc7909a3ad66bd3f72d
unknown <bbabines@gmail.com>
2023-02-08 22:29:24 -0500

    Commiting to clone on laptop

--- src/components/contact-section/ContactSection.jsx
+++ src/components/contact-section/ContactSection.jsx
@@ -1,17 +1,27 @@
 import styles from "./ContactSection.module.css";
+import { Container, Col, Row } from "react-bootstrap";
 
 const ContactSection = () => {
 	return (
 		<div className={styles.container}>
 			<div className={styles.formContainer}>
-				<h1>Contact</h1>
-				<h3>Form</h3>
-				<p>Name</p>
-				<p>Email</p>
-				<p>Message</p>
+				<Container>
+					<Col>
+						<Row></Row>
+						<textarea placeholder="Message" />
+					</Col>
+				</Container>
 			</div>
 		</div>
 	);
 };
 
 export default ContactSection;
+
+{
+	/* <h1>Contact</h1>
+					<h3>Form</h3>
+					<p>Name</p>
+					<p>Email</p>
+					<p>Message</p> */
+}
