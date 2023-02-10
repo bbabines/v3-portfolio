3d6ca6b485b02c0eb027621950fd4b701cf10dc2
unknown <bbabines@gmail.com>
2023-02-09 21:17:53 -0500

    Added dummy content in hero section but not all the way aligned properly.

--- src/components/hero-section/HeroSection.jsx
+++ src/components/hero-section/HeroSection.jsx
@@ -4,19 +4,23 @@ import { Button } from "react-bootstrap";
 const HeroSection = () => {
 	return (
 		<div className={styles.container}>
-			<div className={styles.textContainer}>
-				<h6>Hi, my name is</h6>
-				<h1>Brad Babines</h1>
-				<p>Welcome to my portfolio!</p>
-				<p>
-					I'm a front-end software engineer that builds digital experiences with
-					React and Three.js
-				</p>
-				<div className={styles.buttonContainer}>
-					<Button variant="info">Check out my resume</Button>
+			<div className={styles.secondContainer}>
+				<div className={styles.textContainer}>
+					<h6>Hi, my name is</h6>
+					<h1>Brad Babines</h1>
+					<p>Welcome to my portfolio!</p>
+					<p>
+						I'm a front-end software engineer that builds digital experiences
+						with React and Three.js
+					</p>
+					<div className={styles.buttonContainer}>
+						<Button variant="info">Check out my resume</Button>
+					</div>
+				</div>
+				<div className={styles.animationContainer}>
+					<h2>3D Element</h2>
 				</div>
 			</div>
-			<h2>3D Element</h2>
 		</div>
 	);
 };
