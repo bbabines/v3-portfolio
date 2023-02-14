c568bc5594f5fffc1a646ec04ee4ed77d3bf8878
unknown <bbabines@gmail.com>
2023-02-14 14:19:30 -0500

    Seperated ProjectCard into PersonalProjectCards and PaidProjectsCards. Outline of ProjectSection finished.

--- src/components/project-card/ProjectCard.jsx
+++ src/components/project-card/ProjectCard.jsx
@@ -1,19 +1,18 @@
 import styles from "./ProjectCard.module.css";
 import { default as projects } from "../project-section/project-data.json";
 
-const ProjectCard = () => {
-	const paidProjects = projects.filter((project) => project.paid === true);
-
+export const PersonalProjectCards = () => {
 	const personalProjects = projects.filter((project) => project.paid === false);
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.cardContainer}>
-				{paidProjects.map((project) => (
+				{personalProjects.map((project) => (
 					<div key={project.id}>
 						<h6 className={styles.cardTitle}>{project.title}</h6>
 						<div className={styles.description}>
-							<p>This is the project description. It goes on and on</p>
+							{/**Make Description into a component and/or part of the data.JSON */}
+							{/* <p>This is the project description. It goes on and on</p> */}
 						</div>
 						<img src={`/${project.image}`} className={styles.cardImage} />
 						<div className={styles.linkContainer}>
@@ -39,25 +38,39 @@ const ProjectCard = () => {
 	);
 };
 
-export default ProjectCard;
+export const PaidProjectCards = () => {
+	const paidProjects = projects.filter((project) => project.paid === true);
 
-{
-	/* <h3 className={styles.personalTitle}>Personal Projects</h3>
-				<div className={styles.personalContainer}>
-					{personalProjects.map((project) => (
-						<div key={project.id}>
-							<h6>{project.title}</h6>
-							<img src={`/${project.image}`} />
-							<div>
-								<a href={project.site} target="_blank">
-									View Live Demo
-								</a>
-							</div>
-							<div>
-								<a href={project.github} target="_blank">
-									View Code
-								</a>
-							</div>
+	return (
+		<div className={styles.container}>
+			<div className={styles.cardContainer}>
+				{paidProjects.map((project) => (
+					<div key={project.id}>
+						<h6 className={styles.cardTitle}>{project.title}</h6>
+						<div className={styles.description}>
+							{/**Make Description into a component and/or part of the data.JSON */}
+							{/* <p>This is the project description. It goes on and on</p> */}
 						</div>
-					))} */
-}
+						<img src={`/${project.image}`} className={styles.cardImage} />
+						<div className={styles.linkContainer}>
+							<a
+								href={project.site}
+								target="_blank"
+								className={styles.demoLink}
+							>
+								View Live Demo
+							</a>
+							<a
+								href={project.github}
+								target="_blank"
+								className={styles.projectLink}
+							>
+								View Code
+							</a>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
--- src/components/project-section/ProjectSection.jsx
+++ src/components/project-section/ProjectSection.jsx
@@ -1,14 +1,11 @@
 import styles from "./ProjectSection.module.css";
-import { Button, Card } from "react-bootstrap";
 
-import { default as projects } from "./project-data.json";
-import ProjectCard from "../project-card/ProjectCard";
+import {
+	PersonalProjectCards,
+	PaidProjectCards,
+} from "../project-card/ProjectCard";
 
 const ProjectSection = () => {
-	// const paidProjects = projects.filter((project) => project.paid === true);
-
-	// const personalProjects = projects.filter((project) => project.paid === false);
-
 	return (
 		<div className={styles.container}>
 			<h2 className={styles.projectTitle}>Projects</h2>
@@ -33,111 +30,12 @@ const ProjectSection = () => {
 			</div>
 			<div className={styles.projectsContainer}>
 				<h3 className={styles.freelanceTitle}>Freelance Work</h3>
-				<ProjectCard />
-
-				{/* {paidProjects.map((project) => (
-					<div key={project.id}>
-						<h6>{project.title}</h6>
-						<img src={`/${project.image}`} />
-						<div>
-							<a href={project.site} target="_blank">
-								View Live Demo
-							</a>
-						</div>
-						<div>
-							<a href={project.github} target="_blank">
-								View Code
-							</a>
-						</div>
-					</div>
-				))} */}
-
-				{/** Create Card Component  */}
-				{/* <div className={styles.freelanceContainer}>
-					<Card style={{ width: "20rem", height: "30rem" }}>
-						<Card.Img
-							className={styles.cardImage}
-							variant="top"
-							src="/brads-clothing-thumb.png"
-						/>
-						<Card.Body>
-							<Card.Title>Brad's Clothing</Card.Title>
-							<Card.Text>
-								This is an e-commerce clothing store made with React that has
-								user authentication and database support via firebase.
-							</Card.Text>
-							<Button variant="primary">Live Demo</Button> {""}
-							<Button variant="secondary">Check Out The Code</Button>
-						</Card.Body>
-					</Card>
-				</div> */}
-				{/** End Card Component  */}
-
-				{/* <h3 className={styles.personalTitle}>Personal Projects</h3>
-				<div className={styles.personalContainer}>
-					{personalProjects.map((project) => (
-						<div key={project.id}>
-							<h6>{project.title}</h6>
-							<img src={`/${project.image}`} />
-							<div>
-								<a href={project.site} target="_blank">
-									View Live Demo
-								</a>
-							</div>
-							<div>
-								<a href={project.github} target="_blank">
-									View Code
-								</a>
-							</div>
-						</div>
-					))} */}
-
-				{/* <Card style={{ width: "20rem", height: "30rem" }}>
-						<Card.Img
-							className={styles.cardImage}
-							variant="top"
-							src="/brads-clothing-thumb.png"
-						/>
-						<Card.Body>
-							<Card.Title>Brad's Clothing</Card.Title>
-							<Card.Text>
-								This is an e-commerce clothing store made with React that has
-								user authentication and database support via firebase.
-							</Card.Text>
-							<Button variant="primary">Live Demo</Button> {""}
-							<Button variant="secondary">Check Out The Code</Button>
-						</Card.Body>
-					</Card> */}
-				{/* </div> */}
+				<PaidProjectCards />
+				<h3 className={styles.personalTitle}>Personal Projects</h3>
+				<PersonalProjectCards />
 			</div>
 		</div>
 	);
 };
 
 export default ProjectSection;
-
-{
-	/* 
-	<h3>Freelance Work</h3>
-	<h3>Personal Projects</h3>
-	<h5>Button Filters</h5>
-	<h6>Large, Medium, or Small</h6>
-	<h6>React, Three.js, Vanilla JS, HTML/CSS</h6>
-	<p>Live Demo</p> 
-	
-
-	CARD 
-	1. Remove description and expand picture in the Card.
-	2. Create a hover effect that displays the description
-	3. Create a data file that will map over the info and build cards.
-	
-	FILTER
-	1. Create filtering buttons and generate a overall style of the filtering section
-	2. Create the logic to filter the projects with conditionally rendering the sections.
-	
-	
-	
-	
-	
-	*/
-}
