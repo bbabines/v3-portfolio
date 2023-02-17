9bfd1e8171b519c505486a494729d61c2460f530
unknown <bbabines@gmail.com>
2023-02-16 22:11:09 -0500

    Fixed project cards.

--- src/components/project-card/ProjectCard.jsx
+++ src/components/project-card/ProjectCard.jsx
@@ -1,20 +1,22 @@
 import styles from "./ProjectCard.module.css";
 import { default as projects } from "../project-section/project-data.json";
 
-export const PersonalProjectCards = () => {
-	const personalProjects = projects.filter((project) => project.paid === false);
+export const PaidProjectCards = () => {
+	const paidProjects = projects.filter((project) => project.paid === true);
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.cardContainer}>
-				{personalProjects.map((project) => (
+				{paidProjects.map((project) => (
 					<div key={project.id}>
 						<h6 className={styles.cardTitle}>{project.title}</h6>
 						<div className={styles.description}>
 							{/**Make Description into a component and/or part of the data.JSON */}
 							{/* <p>This is the project description. It goes on and on</p> */}
 						</div>
-						<img src={`/${project.image}`} className={styles.cardImage} />
+						<div className={styles.imageContainer}>
+							<img src={`/${project.image}`} className={styles.cardImage} />
+						</div>
 						<div className={styles.linkContainer}>
 							<a
 								href={project.site}
@@ -38,20 +40,22 @@ export const PersonalProjectCards = () => {
 	);
 };
 
-export const PaidProjectCards = () => {
-	const paidProjects = projects.filter((project) => project.paid === true);
+export const PersonalProjectCards = () => {
+	const personalProjects = projects.filter((project) => project.paid === false);
 
 	return (
 		<div className={styles.container}>
 			<div className={styles.cardContainer}>
-				{paidProjects.map((project) => (
+				{personalProjects.map((project) => (
 					<div key={project.id}>
 						<h6 className={styles.cardTitle}>{project.title}</h6>
-						<div className={styles.description}>
-							{/**Make Description into a component and/or part of the data.JSON */}
-							{/* <p>This is the project description. It goes on and on</p> */}
+						{/* <div className={styles.description}> */}
+						{/**Make Description into a component and/or part of the data.JSON */}
+						{/* <p>This is the project description. It goes on and on</p> */}
+						{/* </div> */}
+						<div className={styles.imageContainer}>
+							<img src={`/${project.image}`} className={styles.cardImage} />
 						</div>
-						<img src={`/${project.image}`} className={styles.cardImage} />
 						<div className={styles.linkContainer}>
 							<a
 								href={project.site}
