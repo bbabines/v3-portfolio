563d614104c8fe90261645b4589bb6f490565c52
unknown <bbabines@gmail.com>
2023-02-18 23:46:28 -0500

    Added conditional rendering to buttons.

--- src/components/project-card/ProjectCard.jsx
+++ src/components/project-card/ProjectCard.jsx
@@ -2,7 +2,6 @@ import styles from "./ProjectCard.module.css";
 import { default as projects } from "../project-section/project-data.json";
 import { Button } from "react-bootstrap";
 import { useState } from "react";
-import { FaLess } from "react-icons/fa";
 
 export const ProjectCards = () => {
 	const projectsData = projects;
@@ -41,250 +40,118 @@ export const ProjectCards = () => {
 		<div className={styles.container}>
 			<div>
 				<div>
-					<button onClick={() => handleSizeFilter("all")}>All</button>
-					<button onClick={() => handleSizeFilter("small")}>
-						Small Projects
-					</button>
-					<button onClick={() => handleSizeFilter("medium")}>
-						Medium Projects
-					</button>
-					<button onClick={() => handleSizeFilter("large")}>
-						Large Projects
-					</button>
+					Technology:
+					<Button
+						className={
+							techFilter === "all" ? "btn btn-secondary" : "btn btn-primary"
+						}
+						onClick={() => handleTechFilter("all")}
+					>
+						All
+					</Button>
+					<Button
+						className={
+							techFilter === "react" ? "btn btn-secondary" : "btn btn-primary"
+						}
+						onClick={() => handleTechFilter("react")}
+					>
+						React
+					</Button>
+					<Button
+						className={
+							techFilter === "three" ? "btn btn-secondary" : "btn btn-primary"
+						}
+						onClick={() => handleTechFilter("three")}
+					>
+						Three.js
+					</Button>
+					<Button
+						className={
+							techFilter === "js" ? "btn btn-secondary" : "btn btn-primary"
+						}
+						onClick={() => handleTechFilter("js")}
+					>
+						JavaScript
+					</Button>
+					<Button
+						className={
+							techFilter === "html" ? "btn btn-secondary" : "btn btn-primary"
+						}
+						onClick={() => handleTechFilter("html")}
+					>
+						HTML
+					</Button>
 				</div>
 				<div>
-					<button onClick={() => handleTechFilter("all")}>All</button>
-					<button onClick={() => handleTechFilter("react")}>React</button>
-					<button onClick={() => handleTechFilter("three")}>Three.js</button>
-					<button onClick={() => handleTechFilter("js")}>JavaScript</button>
-					<button onClick={() => handleTechFilter("html")}>HTML</button>
+					Size:
+					<Button
+						className={
+							sizeFilter === "all" ? "btn btn-secondary" : "btn btn-primary"
+						}
+						onClick={() => handleSizeFilter("all")}
+					>
+						All
+					</Button>
+					<Button
+						className={
+							sizeFilter === "large" ? "btn btn-secondary" : "btn btn-primary"
+						}
+						onClick={() => handleSizeFilter("large")}
+					>
+						Large Projects
+					</Button>
+					<Button
+						className={
+							sizeFilter === "medium" ? "btn btn-secondary" : "btn btn-primary"
+						}
+						onClick={() => handleSizeFilter("medium")}
+					>
+						Medium Projects
+					</Button>
+					<Button
+						className={
+							sizeFilter === "small" ? "btn btn-secondary" : "btn btn-primary"
+						}
+						onClick={() => handleSizeFilter("small")}
+					>
+						Small Projects
+					</Button>
 				</div>
-				<ul>
+
+				<div className={styles.cardContainer}>
 					{filteredProjects.map((project) => (
-						<li key={project.id}>
-							<h2>{project.title}</h2>
-							<img src={project.image} alt={project.title} />
-							<p>Size: {project.size}</p>
-							<p>Technology: {project.technology}</p>
-							<a href={project.site}>Live Site</a>
-							<a href={project.github}>GitHub Repo</a>
-						</li>
+						<div key={project.id}>
+							<h6 className={styles.cardTitle}>{project.title}</h6>
+							<div className={styles.description}>
+								{/**Make Description into a component and/or part of the data.JSON */}
+								{/* <p>This is the project description. It goes on and on</p> */}
+							</div>
+							<div className={styles.imageContainer}>
+								<img src={`/${project.image}`} className={styles.cardImage} />
+							</div>
+
+							<div className={styles.linkContainer}>
+								<a
+									href={project.site}
+									target="_blank"
+									className={styles.demoLink}
+								>
+									Live Demo
+								</a>
+								<a
+									href={project.github}
+									target="_blank"
+									className={styles.projectLink}
+								>
+									View Code
+								</a>
+							</div>
+						</div>
 					))}
-				</ul>
+				</div>
 			</div>
 		</div>
 	);
 };
 
-// const [largeFilterState, setLargeFilterState] = useState(false);
-// const largeFilter = allProjects.filter((project) => project.size === "large");
-// const toggleLargeFilter = () => {
-// 	setLargeFilterState((prevFilter) => !prevFilter);
-// };
-
-// const [mediumFilterState, setMediumFilterState] = useState(false);
-// const mediumFilter = allProjects.filter(
-// 	(project) => project.size === "medium"
-// );
-// const toggleMediumFilter = () => {
-// 	setMediumFilterState((prevFilter) => !prevFilter);
-// };
-
-// const [smallFilterState, setSmallFilterState] = useState(false);
-// const smallFilter = allProjects.filter((project) => project.size === "small");
-// const toggleSmallFilter = () => {
-// 	setSmallFilterState((prevFilter) => !prevFilter);
-// };
-
-// const [paidFilterState, setPaidFilterState] = useState(false);
-// const paidFilter = projects.filter((project) => project.paid === true);
-// const togglePaidFilter = () => {
-// 	setPaidFilterState((prevFilter) => !prevFilter);
-// };
-
-// const [personalFilterState, setPersonalFilterState] = useState(false);
-// const personalFilter = projects.filter((project) => project.paid === false);
-// const togglePersonalFilter = () => {
-// 	setPersonalFilterState((prevFilter) => !prevFilter);
-// };
-
 // className={largeFilterState ? "btn btn-primary" : "btn btn-secondary"}
-// onClick={toggleLargeFilter}
-{
-	/* <Button
-				className={mediumFilterState ? "btn btn-primary" : "btn btn-secondary"}
-				onClick={toggleMediumFilter}
-			>
-				Test Medium Filter
-			</Button>
-			<Button onClick={toggleSmallFilter}>Test Small Filter</Button>
-			<Button onClick={togglePaidFilter}>Paid</Button>
-			<Button onClick={togglePersonalFilter}>Personal</Button> */
-}
-
-// {
-// 	largeFilterState && (
-// 		<div className={styles.cardContainer}>
-// 			{largeFilter.map((project) => (
-// 				<div key={project.id}>
-// 					<h6 className={styles.cardTitle}>{project.title}</h6>
-// 					<div className={styles.description}>
-// 						{/**Make Description into a component and/or part of the data.JSON */}
-// 						{/* <p>This is the project description. It goes on and on</p> */}
-// 					</div>
-// 					<div className={styles.imageContainer}>
-// 						<img src={`/${project.image}`} className={styles.cardImage} />
-// 					</div>
-// 					<div className={styles.linkContainer}>
-// 						<a href={project.site} target="_blank" className={styles.demoLink}>
-// 							Live Demo
-// 						</a>
-// 						<a
-// 							href={project.github}
-// 							target="_blank"
-// 							className={styles.projectLink}
-// 						>
-// 							View Code
-// 						</a>
-// 					</div>
-// 				</div>
-// 			))}
-// 		</div>
-// 	);
-// }
-
-// {
-// 	personalFilterState && (
-// 		<div className={styles.cardContainer}>
-// 			{personalFilter.map((project) => (
-// 				<div key={project.id}>
-// 					<h6 className={styles.cardTitle}>{project.title}</h6>
-// 					<div className={styles.description}>
-// 						{/**Make Description into a component and/or part of the data.JSON */}
-// 						{/* <p>This is the project description. It goes on and on</p> */}
-// 					</div>
-// 					<div className={styles.imageContainer}>
-// 						<img src={`/${project.image}`} className={styles.cardImage} />
-// 					</div>
-// 					<div className={styles.linkContainer}>
-// 						<a href={project.site} target="_blank" className={styles.demoLink}>
-// 							Live Demo
-// 						</a>
-// 						<a
-// 							href={project.github}
-// 							target="_blank"
-// 							className={styles.projectLink}
-// 						>
-// 							View Code
-// 						</a>
-// 					</div>
-// 				</div>
-// 			))}
-// 		</div>
-// 	);
-// }
-
-// {
-// 	mediumFilterState && (
-// 		<div className={styles.cardContainer}>
-// 			{mediumFilter.map((project) => (
-// 				<div key={project.id}>
-// 					<h6 className={styles.cardTitle}>{project.title}</h6>
-// 					<div className={styles.description}>
-// 						{/**Make Description into a component and/or part of the data.JSON */}
-// 						{/* <p>This is the project description. It goes on and on</p> */}
-// 					</div>
-// 					<div className={styles.imageContainer}>
-// 						<img src={`/${project.image}`} className={styles.cardImage} />
-// 					</div>
-// 					<div className={styles.linkContainer}>
-// 						<a
-// 							href={project.site}
-// 							target="_blank"
-// 							className={styles.demoLink}
-// 						>
-// 							Live Demo
-// 						</a>
-// 						<a
-// 							href={project.github}
-// 							target="_blank"
-// 							className={styles.projectLink}
-// 						>
-// 							View Code
-// 						</a>
-// 					</div>
-// 				</div>
-// 			))}
-// 		</div>
-// 	);
-// }
-
-// {
-// 	smallFilterState && (
-// 		<div className={styles.cardContainer}>
-// 			{smallFilter.map((project) => (
-// 				<div key={project.id}>
-// 					<h6 className={styles.cardTitle}>{project.title}</h6>
-// 					<div className={styles.description}>
-// 						{/**Make Description into a component and/or part of the data.JSON */}
-// 						{/* <p>This is the project description. It goes on and on</p> */}
-// 					</div>
-// 					<div className={styles.imageContainer}>
-// 						<img src={`/${project.image}`} className={styles.cardImage} />
-// 					</div>
-// 					<div className={styles.linkContainer}>
-// 						<a
-// 							href={project.site}
-// 							target="_blank"
-// 							className={styles.demoLink}
-// 						>
-// 							Live Demo
-// 						</a>
-// 						<a
-// 							href={project.github}
-// 							target="_blank"
-// 							className={styles.projectLink}
-// 						>
-// 							View Code
-// 						</a>
-// 					</div>
-// 				</div>
-// 			))}
-// 		</div>
-// 	);
-// }
-
-// {
-// 	paidFilterState && (
-// 		<div className={styles.cardContainer}>
-// 			{paidFilter.map((project) => (
-// 				<div key={project.id}>
-// 					<h6 className={styles.cardTitle}>{project.title}</h6>
-// 					<div className={styles.description}>
-// 						{/**Make Description into a component and/or part of the data.JSON */}
-// 						{/* <p>This is the project description. It goes on and on</p> */}
-// 					</div>
-// 					<div className={styles.imageContainer}>
-// 						<img src={`/${project.image}`} className={styles.cardImage} />
-// 					</div>
-// 					<div className={styles.linkContainer}>
-// 						<a
-// 							href={project.site}
-// 							target="_blank"
-// 							className={styles.demoLink}
-// 						>
-// 							Live Demo
-// 						</a>
-// 						<a
-// 							href={project.github}
-// 							target="_blank"
-// 							className={styles.projectLink}
-// 						>
-// 							View Code
-// 						</a>
-// 					</div>
-// 				</div>
-// 			))}
-// 		</div>
-// 	);
-// }
