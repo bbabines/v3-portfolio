7773fb9ac3427fe8e53119e36946eef0ee9c729f
unknown <bbabines@gmail.com>
2023-02-20 12:46:43 -0500

    ProjectSection now responsive.

--- src/components/project-section/ProjectSection.jsx
+++ src/components/project-section/ProjectSection.jsx
@@ -13,38 +13,6 @@ const ProjectSection = () => {
 	return (
 		<div id="projects" className={styles.container}>
 			<h2 className={styles.projectTitle}>Projects</h2>
-			{/* <div className={styles.filterContainer}>
-				<div className={styles.technologyFilterContainer}>
-					<p className={styles.filterTitle}>Technology</p>
-					<Button className={styles.filterButtons} type="button">
-						HTML/CSS
-					</Button>
-
-					<Button className={styles.filterButtons} type="button">
-						Vanilla JS
-					</Button>
-
-					<Button className={styles.filterButtons} type="button">
-						Three.js
-					</Button>
-					<Button className={styles.filterButtons} type="button">
-						React
-					</Button>
-				</div>
-
-				<div className={styles.sizeFilterContainer}>
-					<p className={styles.filterTitle}>Size</p>
-					<Button className={styles.filterButtons} type="button">
-						Small Projects
-					</Button>
-					<Button className={styles.filterButtons} type="button">
-						Medium Projects
-					</Button>
-					<Button className={styles.filterButtons} type="button">
-						Large Projects
-					</Button>
-				</div>
-			</div> */}
 
 			<div className={styles.projectsContainer}>
 				{/* <h3 className={styles.freelanceTitle}>Freelance Work</h3> */}
@@ -52,8 +20,6 @@ const ProjectSection = () => {
 					<ProjectCards />
 					{/* <PersonalProjectCards /> */}
 				</div>
-				{/* <h3 className={styles.personalTitle}>Personal Projects</h3>
-				<div className={styles.personalContainer}></div> */}
 			</div>
 		</div>
 	);
